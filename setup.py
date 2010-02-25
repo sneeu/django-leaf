e7fc3291dd7f2ee66aefac625c7b6e7d11d32337
sneeu <john@sneeu.com>
2010-02-25 18:06:47 +0000

    Updates setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, find_packages
+
+setup(
+    name='django-leaf',
+    version='0.1',
+    description='',
+    author='John Sutherland',
+    author_email='john@sneeu.com',
+    url='http://sneeu.com/projects/',
+    packages=find_packages(where='.'))
